478001de467148276b9b57af999fe27e1f5885bc
mariamamunova <mariam493@nycstudents.net>
2025-11-14 15:21:41 -0500

    Update FrogSimulation.java

--- src/FrogSimulation.java
+++ src/FrogSimulation.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
-
 public class FrogSimulation {
+    /** Distance, in inches, from the starting position to the goal. */
+    private int goalDistance;
+
+    /** Maximum number of hops allowed to reach the goal. */
+    private int maxHops;
+
+    /** Costructs a FrogSimulation where dist is the distance, in inches, from the starting
+     * position to the goal, and numHops is the maximum number of hops allowed to reach the goal.
+     * Precondition: dist > 0; numHops > 0
+     */
+    public FrogSimulation(int dist, int numHops){
+        goalDistance = dist;
+        maxHops = numHops;
+    }
+
+    /** Returns an integer representing the distance, in inches, to be moved when the frog hops.
+     */
+    private int hopDistance(){
+        /*implementation not shown */
+        int start = 0;
+
+    }
+
+    /** Simulates a frog attempting to reach the goal as described in part (a).
+     * Returns true if the frog successfully reached or passed the goal during the simulation;
+     *         false otherwise.
+     */
+    public boolean simulate(){
+        /* include the starting position being at 0 */
+        if (goalDistance <= hopDistance())
+                return true;
+        else
+            return false;
+    }
+
+    /** Runs num simulations and returns the proportion of simulations in which the frog
+     * successfully reached or passed the goal.
+     * Precondition: num > 0
+     */
+    public double runSimulations(int num){
+        /* to be implemented in part (b) */
+    }
 }
