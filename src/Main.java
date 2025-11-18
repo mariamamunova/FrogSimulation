80cc12f6aec118662199fdd2506b9572f816635b
mariamamunova <mariam493@nycstudents.net>
2025-11-18 15:15:19 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String []args){
+        FrogSimulation frogsim = new FrogSimulation(24,5);
+        System.out.println(frogsim.hopDistance());
+        System.out.println(frogsim.simulate());
+        System.out.println(frogsim.runSimulations(400));
+    }
 }
